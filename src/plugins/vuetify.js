feb902512ece7873820da3af5e5241131687228c
Kyle Henderson <kyle_henderson@swifttrans.com>
2020-01-03 14:03:30 -0700

    add base knx theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,27 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+const options = {
+	icons: {
+		iconfont: 'mdi',
+		},
+		theme: {
+		themes: {
+			light: {
+				knightRed: '#b10021',
+				knightGold: '#fe9915',
+				knightGrey: '#222222',
+				primary: '#0069bd',
+				secondary: '#424242',
+				accent: '#82B1FF',
+				error: '#FF5252',
+				info: '#0069bd',
+				success: '#4CAF50',
+				warning: '#FFC107',
+			}
+		}
+	}
+}
+
+
+export default new Vuetify(options);
