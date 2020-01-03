b9a9962588107f0da150181a0d64e45dc4202728
Kyle Henderson <kyle_henderson@swifttrans.com>
2020-01-03 14:01:09 -0700

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
-  <div id="app">
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-content>
+    
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'app',
-  components: {
-  }
-}
-</script>
+  name: 'App',
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
